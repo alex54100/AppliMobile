243c153119dfd77bcf4de47e7537a51a3c90db40
alexandre.ajuste@ynov.com <alexandre.ajuste@ynov.com>
2020-02-25 16:36:05 +0100

    add troc

--- screens/AddTroc.js
+++ screens/AddTroc.js
@@ -1,35 +1,34 @@
-import * as React from 'react';
-import {Text, View, TextInput, Button} from "react-native";
-
-export default class AddTroc extends React.Component {
-
-    render() {
-        return(
-            <View>
-                <Text h1 style={{ flex: 1, justifyContent: "center", padding: 32 }}>
-                    Ajouter un troc
-                </Text>
-
-                <View style={{ marginTop: 20 }}>
-                    <Text>
-                        Que recherchez vous ?
-                    </Text>
-                    <TextInput placeholder='Troc recherché'/>
-                </View>
-
-                <View style={{ marginTop: 20 }}>
-                    <Text>
-                        Proposer votre troc
-                    </Text>
-                    <TextInput placeholder='Description de votre troc'/>
-                    <Button title='Ajouter' onPress={() => {}}/>
-                </View>
+import React, { useState } from 'react';
+import { Text, View, ScrollView, StyleSheet, FlatList, Image, Alert, KeyboardAvoidingView } from 'react-native';
+import { Appbar, TextInput, Button } from 'react-native-paper';
+import firebase from '../firebaseInit.js'
+import '@firebase/firestore';
 
 
-
-            </View>
-        )
+function addTroc() {
+    const [troc, setTroc] = useState("");
+    const ref = firebase.firestore().collection('Troc')
+    async function addTroc() {
+        await ref.add({
+            photo: photo,
+            servicedemande: servicedemande,
+            servicepropose: servicepropose,
+        });
+        setTroc("");
     }
 
-
+    return(
+        <View>
+            <Appbar>
+                <Appbar.Content title={'Petits bouts de troc'} />
+            </Appbar>
+            <ScrollView style={{ flex: 1 }}>
+                <Text>Liste des trocs </Text>
+            </ScrollView>
+            <TextInput label={'Service demandé'} value={servicedemande} onChangeText={setTroc} />
+            <TextInput label={'Service proposé'} value={servicepropose} onChangeText={setTroc} />
+            <TextInput label={'Photo'} value={photo} onChangeText={setTroc} />
+            <Button onPress={() => addTroc()}>ajouter un petit bout de troc !</Button>
+        </View>
+    )
 }
