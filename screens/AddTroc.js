890c4fa0ef4f411daaaa023881c82373ba83aa8b
alexandre.ajuste@ynov.com <alexandre.ajuste@ynov.com>
2020-03-10 09:59:19 +0100

    addTroc ready

--- screens/AddTroc.js
+++ screens/AddTroc.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { Text, View, ScrollView, StyleSheet, FlatList, Image, Alert, KeyboardAvoidingView } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { Appbar, TextInput, Button } from 'react-native-paper';
 import firebase from '../firebaseInit.js'
 import '@firebase/firestore';
 
 
-function addTroc() {
-    const [troc, setTroc] = useState("");
+function AddTroc() {
+    const [photo, setPhoto] = useState("");
+    const [servicedemande, setServiceDemande] = useState("");
+    const [servicepropose, setServicePropose] = useState("");
     const ref = firebase.firestore().collection('Troc')
     async function addTroc() {
         await ref.add({
@@ -14,7 +16,9 @@ function addTroc() {
             servicedemande: servicedemande,
             servicepropose: servicepropose,
         });
-        setTroc("");
+        setPhoto("");
+        setServiceDemande("");
+        setServicePropose("");
     }
 
     return(
@@ -25,10 +29,11 @@ function addTroc() {
             <ScrollView style={{ flex: 1 }}>
                 <Text>Liste des trocs </Text>
             </ScrollView>
-            <TextInput label={'Service demandé'} value={servicedemande} onChangeText={setTroc} />
-            <TextInput label={'Service proposé'} value={servicepropose} onChangeText={setTroc} />
-            <TextInput label={'Photo'} value={photo} onChangeText={setTroc} />
+            <TextInput label={'Photo'} value={photo} onChangeText={setPhoto} />
+            <TextInput label={'Service demandé'} value={servicedemande} onChangeText={setServiceDemande} />
+            <TextInput label={'Service proposé'} value={servicepropose} onChangeText={setServicePropose} />
             <Button onPress={() => addTroc()}>ajouter un petit bout de troc !</Button>
         </View>
-    )
+    );
 }
+export default AddTroc
