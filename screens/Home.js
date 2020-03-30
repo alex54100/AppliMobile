5f5771c4f676e4ff3e8b71920b590376ca086d48
alexandre.ajuste@ynov.com <alexandre.ajuste@ynov.com>
2020-03-30 21:20:41 +0200

    home presenation

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 
 export default class Home extends React.Component{
@@ -8,18 +8,23 @@ export default class Home extends React.Component{
             <View>
                 <View>
                     <Text style={{ padding: 30, fontSize: 18 }}>Bienvenue sur Troc Service !</Text>
-                    <Text style> Cette application a pour but de mettre en relation les utilisateurs afin que chacun propose ou répondent a différents trocs. </Text>
+                    <Text> Cette application a pour but de mettre en relation les utilisateurs afin que chacun propose ou répondent a différents trocs. </Text>
+
+                    <Text style={styles.container}> Afin de rechercher un troc qui te correspond, tu peux appuyer sur le bouton si dessous afin de voir tout les trocs disponible sur l'application ! </Text>
+                    <Button title='Rechercher un troc' onPress={() => {}}/>
+
+                    <Text style={styles.container}> Si tu as besoin d'aide pour une demande particulière, tu peux ajouter ton troc avec le bouton si dessous ! </Text>
+                    <Button title='Ajouter un troc' onPress={() => {}}/>
+
                 </View>
-
-
-                    <View style={{ marginTop: 200 }}>
-
-                        <Button title='Mes trocs' onPress={() => {}}/>
-                        <Button title='Rechercher un troc' onPress={() => {}}/>
-
-                    </View>
-
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 25,
+        padding: 5,
+    }
+});
