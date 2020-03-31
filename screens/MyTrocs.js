c4ac99829107c6c8ecb007c06f265ed39bdae3fe
alexandre.ajuste@ynov.com <alexandre.ajuste@ynov.com>
2020-03-31 13:33:54 +0200

    presentation mes trocs

--- screens/MyTrocs.js
+++ screens/MyTrocs.js
@@ -1,37 +1,42 @@
 import * as React from 'react';
-import {Text, View, StyleSheet} from "react-native";
+import {Text, View, StyleSheet, Button} from "react-native";
 
 
 export default class AddTroc extends React.Component {
 
     render() {
         return <View>
-            <Text style={{textAlign: "center", fontSize: 32, marginTop: 40}}>
-                Mes trocs
-            </Text>
+                    <Text style={{textAlign: "center", fontSize: 32, marginTop: 40}}>
+                        Mes trocs
+                    </Text>
                     <View style={styles.container}>
                         <Text style={{textAlign: "left", fontSize: 20, marginTop: 12, marginBottom: 4}}>Service demandé</Text>
                         <Text> Monter un meuble </Text>
 
-                        <Text style={{textAlign: "left", fontSize: 20, marginTop: 12, marginBottom: 4}}>Service propose</Text>
+                        <Text style={{textAlign: "left", fontSize: 20, marginTop: 12, marginBottom: 4}}>Service proposé</Text>
                         <Text> Cours de violon </Text>
 
+                        <View style={styles.fixToText}>
+                            <Button
+                                title="Modifier"
+                            />
+                            <Button
+                                color="#FF0000"
+                                title="Supprimer"
+                            />
+                        </View>
                     </View>
-        </View>
-
-
-
-
+                </View>
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 22
+        paddingTop: 25
     },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
+    fixToText: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingTop: 20,
     },
 })
